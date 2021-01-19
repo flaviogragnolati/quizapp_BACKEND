9043dd49674daca02a68b5bf805e7fc4236b5708
flaviogragnolati <flavio.gragnolat@gmail.com>
2021-01-19 20:40:07 -0300

    setting server

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,37 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const server = require('./server');
+const db = require('./models');
 
-const server = express();
-
-/**
- * SETUP GENERAL
- */
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser(SECRET_KEY));
-server.use(morgan('dev')); //para logear a la terminal
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'localhost:3001');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); //Linea agregada para probar por que no axios.delete no recibe ninguna respuesta
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
+server.listen(3000, function () {
+  db.sequelize.sync();
 });
-server.use(cors({ origin: true, credentials: true }));
-
-// Error catching endware.
-server.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
-
-module.exports = server;
