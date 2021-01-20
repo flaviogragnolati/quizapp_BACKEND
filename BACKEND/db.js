c3dfa19f29a2c08ec24cbab85e868740edcdf05f
Tanito <ulises.d83@gmail.com>
2021-01-20 08:58:25 -0300

    generting migrations subject, perm, quiz, school

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -61,6 +61,10 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const {
+  Permission,
+  Quiz,
+  School,
+  Subject,
   User,
 
 } = sequelize.models;
