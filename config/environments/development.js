6bc5639a6e3f25360b88fc97252d0db81261145a
flaviogragnolati <flavio.gragnolat@gmail.com>
2021-01-19 22:14:40 -0300

    setup db con env vars

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,10 +1,12 @@
 module.exports = {
   PORT: process.env.PORT,
   DB: {
-    username: 'postgres',
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'quizdb',
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    logging: false,
+    native: false,
   },
 };
