f4fa15b97f549fcf4cbcc2596d904e1d9c1b06be
Tanito <ulises.d83@gmail.com>
2021-01-20 17:44:04 -0300

    migrations config

--- config/environments/development.js
+++ config/environments/development.js
@@ -6,7 +6,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    logging: true,
+    logging: false,
     native: false,
   },
   FRONT_URL: process.env.FRONT_URL,
