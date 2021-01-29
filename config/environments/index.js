dd26bb66d3530066cb3a563b6278175cb93cd249
Tanito <ulises.d83@gmail.com>
2021-01-29 19:36:30 -0300

    changing var to development

--- config/environments/index.js
+++ config/environments/index.js
@@ -8,7 +8,7 @@ const PRODUCTION = require('./production');
 const DEVELOPMENT = require('./development');
 const { NODE_ENV } = process.env;
 
-let currentEnv = PRODUCTION;
+let currentEnv = DEVELOPMENT;
 
 if (NODE_ENV === 'production') {
   currentEnv = PRODUCTION;
