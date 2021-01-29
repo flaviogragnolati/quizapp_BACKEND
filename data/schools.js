b16eb058d386dda06e6cf3ef12fbcf69ed48012c
Tanito <ulises.d83@gmail.com>
2021-01-29 18:36:30 -0300

    seeders

---
+++ data/schools.js
@@ -0,0 +1,62 @@
+const schools = [{
+    name: 'Universidad Católica',
+    email: 'example@example.com',
+    description: "Somos una escuela",
+    country: 'Argentina',
+    city: "Buenos Aires",
+    password: "qwerty",
+    address: "alguna calle 123",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdn0tmPcxuM2SaOJFQOiVzIWzzbM3z9kIiFw&usqp=CAU",
+    createdAt: new Date(),
+    updatedAt: new Date()
+},
+  {
+    name: 'Universidad Austral',
+    email: 'example2@example.com',
+    description: "Somos otra escuela",
+    country: 'Argentina',
+    city: "Buenos Aires",
+    password: "qwerty",
+    address: "alguna calle más al sur 123",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Logo_Austral_Negativo_Vertical.jpg/1200px-Logo_Austral_Negativo_Vertical.jpg",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    name: 'Universidad de la República',
+    email: 'example3@example.com',
+    description: "Somos una escuela",
+    country: 'Uruguay',
+    city: "Montevideo",
+    password: "qwerty",
+    address: "alguna calle más al norte 123",
+    logo: "https://lh3.googleusercontent.com/proxy/NOWx5p0sU6lzR6Dh5EEtsjarvuqD2NI77-I3kNQlpH_02_aa-pJeRlBy3x2zM5EXrqwrHxCK1AFmVZQGQeOtxZN0447Ia7t39IKsDs3J",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    name: 'Henry',
+    email: 'example4@example.com',
+    description: "Somos una escuela a distancia",
+    country: 'Argentina',
+    city: "Buenos Aires",
+    password: "qwerty",
+    address: "alguna calle más al este 123",
+    logo: "https://cdn.theorg.com/d3119e0e-8202-4034-85ce-d0356382515e_thumb.jpg",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    name: 'Universidad Autónoma de Chile',
+    email: 'example5@example.com',
+    description: "Somos una escuela",
+    country: 'Chile',
+    city: "Talca",
+    password: "qwerty",
+    address: "alguna calle más al oeste 123",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/b/be/Logo-universidad-autonoma.jpg",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }]
+
+  module.exports = schools;
--- seeders/20210129200135-INFO.js
+++ seeders/20210129200135-INFO.js
@@ -1,24 +1,40 @@
+const users = require ('../data/users'); 
+const schools = require ('../data/schools'); 
+const subjects = require('../data/subjets');
+const questions = require('../data/questions');
+const answers = require ('../data/answers');
+const quizzes = require('../data/quizzes');
+const reviews = require('../data/reviews');
+
+
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.bulkInsert('Users', users , { returning: true, hooks: true, validate: true }),
+      queryInterface.bulkInsert('Schools', schools , { returning: true, hooks: true, validate: true }),
+      queryInterface.bulkInsert('Subjects', subjects , { returning: true, hooks: true, validate: true }),
+      queryInterface.bulkInsert('Questions', questions , { returning: true, hooks: true, validate: true }),
+      queryInterface.bulkInsert('Answers', answers , { returning: true, hooks: true, validate: true }),
+      queryInterface.bulkInsert('Quizzes', quizzes , { returning: true, hooks: true, validate: true }),
+      queryInterface.bulkInsert('Reviews', reviews , { returning: true, hooks: true, validate: true }),
+    ])
   },
 
+
+
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
+      return Promise.all([
+        queryInterface.bulkDelete('Users', null , {  }),
+        queryInterface.bulkDelete('Schools', null , {  }),
+        queryInterface.bulkDelete('Subjects', null , {  }),
+        queryInterface.bulkDelete('Questions', null , {  }),
+        queryInterface.bulkDelete('Answers', null , {  }),
+        queryInterface.bulkDelete('Quizzes', null , {  }),
+        queryInterface.bulkDelete('Reviews', null , {  }),
+      ])
+   
   }
 };
