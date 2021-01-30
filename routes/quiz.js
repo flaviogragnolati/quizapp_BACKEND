973c5b7ba4bb2149928ac0054b7f8059630db9fe
Tanito <ulises.d83@gmail.com>
2021-01-30 14:57:50 -0300

    mega route

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const server = require("express").Router();
-const { Quiz, Role } = require("../models/index");
+const { Quiz, Role, User, Subject, Review, Question, Answer, School, QuizTag } = require("../models/index");
 const { checkSuperAdmin } = require("../utils/authTools.js");
 
 // Borrar una QUIZ by ID - DELETE a /quiz/:id
@@ -35,22 +35,163 @@ server.delete(
   }
 );
 
-// Traer un quiz - GET a /quiz/:id
+// Traer todos los quizzes - GET a /quiz
+// La School, la subject, tags, reviews
+//En vez de cantidad de estudiantes poner el promedio de la review?
+server.get("/", async (req, res) => {
+  //Agregar el tag dentro del objeto de cada quiz.
+  
+ try {
+   const schools = await School.findAll();
+   
+   const subjects = await Subject.findAll();
 
+   const quizzes = await Quiz.findAll();
+    
+   const quizTags = await QuizTag.findAll();
+   
+   const reviews = await Review.findAll();
+
+  let response = {
+    schools,
+    subjects,
+    quizzes,
+    quizTags,
+    reviews,
+  };
+
+  for(let prop in response) {
+    response[prop].byId = [prop];
+    response[prop].allIds = [prop].map(p => {
+      return p.id
+    })
+  };
+
+  /*response.schools.byId = schools;
+  response.schools.allIds = schools.map(s => {
+    return s.id;
+  });
+
+  response.subjects.byId = subjects;
+  response.subjects.allIds = subjects.map(sj => {
+    return sj.id;
+  });
+
+  response.quizzes.byId = quizzes;
+  response.quizzes.allIds = quizzes.map(q => {
+    return q.id;
+  });
+
+  response.quizTags.byId = quizTags;
+  response.quizTags.allIds = quizTags.map(qt => {
+    return qt.id;
+  })
+
+  response.reviews.byId = reviews;
+  response.reviews.allIds = reviews.map(r => {
+    return r.id;
+  });*/
+  
+  return res.status(200).send(response);
+  
+  } catch(error) {
+    console.error(error);
+    return res.status(500).send({ message: "Error al buscar los quizzes" });
+  }
+});
+
+
+// Traer un quiz - GET a /quiz/:id
+// Los profes, la School, la subject, tags, preguntas, reviews. Y alumnos??? Aunque no se muestren.
 server.get("/:id", async (req, res) => {
   let { id } = req.params;
+  
   if (!id)
     return res.status(400).send("Debe indicar el id del quiz que desea buscar");
-  Quiz.findOne({
-    where: { id },
-  })
-    .then((quiz) => {
-      return res.status(200).send(quiz);
-    })
-    .catch((error) => {
-      console.error(error);
-      return res.status(500).send({ message: "Error al buscar el quiz" });
-    });
+
+    try {
+      const quiz = await Quiz.findOne({
+        where: { id }
+      });
+     
+      const schools = await School.findOne({
+        where: { id: quiz.SchoolId }
+      });
+      
+      const subject = await Subject.findOne({
+        where: { id: quiz.subjectId }
+      });
+    
+      const quizTags = await QuizTag.findAll({
+        where: { QuizId: quiz.id }
+      });
+      
+      const reviews = await Review.findAll({
+        where: { QuizId: quiz.id }
+      });
+
+      const teachers = await Role.findAll({
+        where: {
+          QuizId: id,
+          name: "Teacher",
+        }
+      });
+
+/*       teachers = []
+
+      const usersTeachers = await Userver.findAll({
+        where: { id }
+      }) */
+
+      const questions = await Question.findAll({
+        where: {
+          QuizId: id,
+         }
+      })
+   
+     let response = {
+       schools: {},
+       subjects: {},
+       quiz: {},
+       quizTags: {},
+       reviews: {},
+       teachers: {},
+     };
+
+     response.quiz = quiz;
+   
+     response.schools.byId = schools;
+     response.schools.allIds = schools.map(s => {
+       return s.id;
+     });
+   
+     response.subjects.byId = subjects;
+     response.subjects.allIds = subjects.map(sj => {
+       return sj.id;
+     });
+   
+     response.quizTags.byId = quizTags;
+     response.quizTags.allIds = quizTags.map(qt => {
+       return qt.id;
+     })
+   
+     response.reviews.byId = reviews;
+     response.reviews.allIds = reviews.map(r => {
+       return r.id;
+     });
+
+     response.teachers.byId = teachers;
+     response.teachers.allIds = teachers.map(t => {
+       return t.id;
+     });
+     
+     return res.status(200).send(response);
+     
+     } catch(error) {
+       console.error(error);
+       return res.status(500).send({ message: "Error al buscar el quiz" });
+     }
+
 });
 
 // Traer todos los teachers de un QUIZ - GET a /quiz/:QuizId/teachers
