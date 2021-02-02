91219abc5b31ad39c90e00445d0f78e6e458603e
Tanito <ulises.d83@gmail.com>
2021-02-02 12:39:03 -0300

    photo user by default

--- models/user.js
+++ models/user.js
@@ -53,6 +53,7 @@ module.exports = (sequelize, DataTypes) => {
           isURL: true,
         },
         allowNull: false,
+        default: "https://lh5.googleusercontent.com/-NVHdsx0r0Xk/TYyS1Qen3JI/AAAAAAAAAGU/AMvdDulXehs/s1600/zarpas.png"
       },
       password: {
         type: DataTypes.STRING,
