b9622d0d3d06eaebebcf897c12bc825450274cd4
Tanito <ulises.d83@gmail.com>
2021-02-05 12:30:32 -0300

    changing production config

--- config/config.js
+++ config/config.js
@@ -21,7 +21,24 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
-  }
+    dialect: 'postgres',
+    logging: false,
+    native: false,
+    use_env_variable: process.env.DATABASE_URL,
+    pool: {
+      max: 3,
+      min: 1,
+      idle: 10000,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        // Ref.: https://github.com/brianc/node-postgres/issues/2009
+        rejectUnauthorized: false,
+      },
+      keepAlive: true,
+    }
+   }
 }
