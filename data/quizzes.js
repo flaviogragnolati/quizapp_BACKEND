34fe87929ef978552b339b44daf65735df8a319b
Tanito <ulises.d83@gmail.com>
2021-02-10 22:20:19 -0300

    modificando data

--- data/quizzes.js
+++ data/quizzes.js
@@ -1,31 +1,5 @@
 const quizzes = [{
 
-  // 3
-  quantity: 10,
-  name: "Quiz de Informática",  // Hay A&Q
-  description: "Con este curso podrás afianzar tus competencias digitales, que son necesarias para desarrollar funciones que impliquen el uso de tecnologías computacionales.",
-  modifiedBy: 2,
-  createdBy: 2,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  SchoolId: 1,
-  SubjectId: 1,
-  logo: 'https://i.ibb.co/GCV3cq9/kkkk.png',
-},
-{
-  // 4
-  quantity: 5,
-  name: "Quiz de CSS",
-  description: "Este curso evaluará tus competencias en el área de diseño web con lenguaje CSS.",
-  modifiedBy: 2,
-  createdBy: 2,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  SchoolId: 1,
-  SubjectId: 1,
-  logo: 'https://i.ibb.co/JHLrnBR/Css-and-HTML-programming-languages-Computer-programming-coding-IT-Female-programmer-cartoon-characte.jpg',
-},
-const quizzes = [{
   // 1
   quantity: 5,
   name: "Quiz de Historia",   // Hay A&Q
