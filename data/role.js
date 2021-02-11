2a34b63a44dbdd800fd045a6cb1e37fa74583d99
Tanito <ulises.d83@gmail.com>
2021-02-10 22:04:05 -0300

    modificando data

--- data/role.js
+++ data/role.js
@@ -19,62 +19,7 @@ const roles = [{
     QuizId: 4,
     UserId: 2,
 },
-{
-    name: "Teacher",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    QuizId: 5,
-    UserId: 2,
-},
-{
-    name: "Teacher",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    QuizId: 1,
-    UserId: 4,
-},
-{
-    name: "Enrolled",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    QuizId: 4,
-    UserId: 3,
-},
-{
-    name: "Teacher",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    QuizId: 3,
-    UserId: 3,
-},
-{
-    name: "Student",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    QuizId: 20,
-    UserId: 2,
-},
-{
-    name: "Student",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    QuizId: 25,
-    UserId: 2,
-},
-{
-    name: "Enrolled",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    QuizId: 11,
-    UserId: 2,
-},
-{
-    name: "Student",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    QuizId: 7,
-    UserId: 2,
-},
+
 ]
 
 module.exports = roles;
